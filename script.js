fe6658c2d523a74b82ea641d2666b4737e38f707
ChaurasiaJayant <chaurasiajayant087@gmail.com>
2026-04-24 11:28:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 //your JS code here. If required.
-const h1 = document.querySelector('h1')
+// const h1 = document.querySelector('h1')
 
-h1.innerText = `Width:${window.innerWidth} and Height:${window.innerHeight}`
+// h1.innerText = `Width:${window.innerWidth} and Height:${window.innerHeight}`
+
+const w= document.querySelector('.w')
+const h = document.querySelector('.h')
+
+window.addEventListener('resize',()=>{
+	w.textContent = window.innerWidth;
+	h.innerContent = window.innerHeight;
+})
